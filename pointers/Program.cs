b08aabde20b0ebee1233bf0c54c4f52bc6f62422
Igor Strupinskiy <Igor Strupinskiy>
2017-06-27 23:02:39 -0700

    initial commit

---
+++ pointers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pointers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            unsafe
+            {
+                int[] MyArray = new int[] { 3, 12, 7, 9 ,2, 99,15,1,2,3,4,5 };
+
+
+                fixed (int* pMyArray = &MyArray[0])
+                {
+                    Console.WriteLine(
+                        (*(pMyArray+5)));
+                    Console.ReadLine();
+                }
+
+            }
+
+
+        }
+    }
+}
